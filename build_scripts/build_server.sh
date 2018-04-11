e6c658d2af8d08c09325715c296ad9deeb8eaa1d
Justin Love <git@JustinLove.name>
2018-04-11 10:17:21 -0500

    turn off forgetting unseen map data

--- build_scripts/build_server.sh
+++ build_scripts/build_server.sh
@@ -15,4 +15,5 @@ echo 0 > settings/requireTicketServerCheck.ini
 echo 0 > settings/useStatsServer.ini
 echo 0 > settings/allowBugReports.ini
 echo 0 > settings/apocalypsePossible.ini
+echo 0 > settings/mapCellForgottenSeconds.ini
 echo 1 > settings/forceEveLocation.ini
