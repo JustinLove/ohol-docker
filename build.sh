2d9f7443a254946718bbf1f12e16a33f2e13df7c
Justin Love <git@JustinLove.name>
2018-04-11 12:14:26 -0500

    add version tags to images

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker build -t ohol-build -f build.Dockerfile build_context
+serverVersion=`cat build_context/source/OneLife/server/serverCodeVersionNumber.txt`
+dataVersion=`cat build_context/source/OneLife/server/serverCodeVersionNumber.txt`
+docker build -t ohol-build:s${serverVersion}d${dataVersion} -f build.Dockerfile build_context
--- server.sh
+++ server.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker build -t ohol-server -f server.Dockerfile server_context
+serverVersion=`cat build_context/source/OneLife/server/serverCodeVersionNumber.txt`
+dataVersion=`cat build_context/source/OneLife/server/serverCodeVersionNumber.txt`
+docker build -t ohol-server:s${serverVersion}d${dataVersion} -f server.Dockerfile server_context
