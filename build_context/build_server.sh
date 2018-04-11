7b9dc0a7877fd6128d0a9d1281a441ee4aa22164
Justin Love <git@JustinLove.name>
2018-04-11 11:33:03 -0500

    disable talking to reflector

--- build_context/build_server.sh
+++ build_context/build_server.sh
@@ -16,4 +16,5 @@ echo 0 > settings/useStatsServer.ini
 echo 0 > settings/allowBugReports.ini
 echo 0 > settings/apocalypsePossible.ini
 echo 0 > settings/mapCellForgottenSeconds.ini
+> settings/reflectorURL.ini
 echo 1 > settings/forceEveLocation.ini
