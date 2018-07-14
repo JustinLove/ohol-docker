40d66cd0508221c8019569164bd925acd96aae1e
Justin Love <git@JustinLove.name>
2018-07-14 08:59:19 -0500

    apparently did not commit tutorial maps last week

--- build_context/build_server.sh
+++ build_context/build_server.sh
@@ -7,6 +7,7 @@ make
 ln -s ../../OneLifeData7/categories .
 ln -s ../../OneLifeData7/objects .
 ln -s ../../OneLifeData7/transitions .
+ln -s ../../OneLifeData7/tutorialMaps .
 ln -s ../../OneLifeData7/dataVersionNumber.txt .
 
 git for-each-ref --sort=-creatordate --format '%(refname:short)' --count=1 refs/tags/OneLife_v* | sed -e 's/OneLife_v//' > serverCodeVersionNumber.txt
