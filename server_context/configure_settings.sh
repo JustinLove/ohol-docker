3f59f50968db7081b1684ec4a2b9837ea2f3b7c0
Justin Love <git@JustinLove.name>
2018-11-28 09:51:32 -0500

    never commited some setting updates

--- server_context/configure_settings.sh
+++ server_context/configure_settings.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
+echo 0 > settings/useCurserServer.ini
+> settings/curseServerURL.ini
 echo 0 > settings/requireTicketServerCheck.ini
+> settings/ticketServerURL.ini
 echo 0 > settings/useStatsServer.ini
+> settings/reflectorURL.ini
 echo 0 > settings/useLineageServer.ini
+> settings/lineageServerURL.ini
 echo 0 > settings/allowBugReports.ini
 echo 0 > settings/apocalypsePossible.ini
 echo 0 > settings/mapCellForgottenSeconds.ini
+echo 1 > settings/skipLookTimeCleanup.ini
 echo 1 > settings/flushLookTimes.ini
-> settings/reflectorURL.ini
 echo 1 > settings/forceEveLocation.ini
 echo 20.0 > settings/minFoodDecrementSeconds.ini
 echo 200.0 > settings/maxFoodDecrementSeconds.ini
