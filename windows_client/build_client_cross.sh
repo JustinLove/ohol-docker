8ee6c736d96211c7c863164ed4d095c598386961
Justin Love <git@JustinLove.name>
2019-01-04 10:17:22 -0600

    add patch readme and changelog to zip

--- windows_client/build_client_cross.sh
+++ windows_client/build_client_cross.sh
@@ -9,6 +9,8 @@ cd gameSource
 make
 
 mv OneLife WonLife.exe
+cp ../patches-master/README.md ./
+cp ../patches-master/CHANGELOG.md ./
 gameClientVersion=$(grep versionNumber game.cpp | head -1 | sed -e 's/[^0-9]*//g' );
 patchVersion=$(cat ../patches-master/patchVersionNumber.txt);
-7z a ../../WonLife-${gameClientVersion}.${patchVersion}.zip WonLife.exe
+7z a ../../WonLife-${gameClientVersion}.${patchVersion}.zip WonLife.exe README.md CHANGELOG.md
