b7af9712c8674462a984cfab43ab0a45a33369bc
Justin Love <git@JustinLove.name>
2019-02-11 10:18:35 -0600

    add name files to zip

--- windows_client/build_client_cross.sh
+++ windows_client/build_client_cross.sh
@@ -11,6 +11,9 @@ make
 mv OneLife WonLife.exe
 cp ../patches-master/README.md ./
 cp ../patches-master/CHANGELOG.md ./
+cp ../server/firstNames.txt ./
+cp ../server/lastNames.txt ./
+cp ../server/namesInfo.txt ./
 gameClientVersion=$(grep versionNumber game.cpp | head -1 | sed -e 's/[^0-9]*//g' );
 patchVersion=$(cat ../patches-master/patchVersionNumber.txt);
-7z a ../../WonLife-${gameClientVersion}.${patchVersion}.zip WonLife.exe README.md CHANGELOG.md
+7z a ../../WonLife-${gameClientVersion}.${patchVersion}.zip WonLife.exe README.md CHANGELOG.md firstNames.txt lastNames.txt namesInfo.txt
