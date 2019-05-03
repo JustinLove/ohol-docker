6be28fedfe4bc51e3b9186e926c0de216314d962
Justin Love <git@JustinLove.name>
2019-05-02 21:28:01 -0500

    override settings on docker machine

--- machine_run.sh
+++ machine_run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+#note: docker-machine scp public_settings.sh wondible-ohol:
 eval $(docker-machine env wondible-ohol)
-docker run --rm --mount source=oholdata1,target=/ohol/db -p 8005:8005 wondible/ohol-server $1
+docker run --rm \
+  --mount source=oholdata1,target=/ohol/db \
+  --mount type=bind,source=/home/ubuntu/public_settings.sh,target=/ohol/configure_settings.sh \
+  -p 8005:8005 wondible/ohol-server $1
