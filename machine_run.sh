a12dfc08e5e179afd6858edc6c96a78a2961f267
Justin Love <git@JustinLove.name>
2019-05-05 10:09:47 -0500

    run script tweaks

--- machine_run.sh
+++ machine_run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #note: docker-machine scp public_settings.sh wondible-ohol:
 eval $(docker-machine env wondible-ohol)
-docker run --rm \
+docker run --rm -d \
   --mount source=oholdata1,target=/ohol/db \
   --mount type=bind,source=/home/ubuntu/public_settings.sh,target=/ohol/configure_settings.sh \
   -p 8005:8005 wondible/ohol-server $1
